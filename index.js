bef25bf6d5a1fd3f9778f3cd82b5494be4cd654c
fahimuddinmoon <fahimmoon103@gmail.com>
2025-07-13 11:58:47 +0600

    some issue solve

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const usersCollection = client.db('collegeApp').collection('users');
         const collegeCollection = client.db('collegeApp').collection('colleges');
         const applyCollection = client.db('collegeApp').collection('apply');
@@ -131,7 +131,7 @@ async function run() {
             res.send(result)
         })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
